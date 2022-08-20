0d9437316589cd9ed5e98b8df6873ab6141dae1d
Wahyu Kurniawan <wahyukurniauan@gmail.com>
2022-08-21 06:47:53 +0700

    first commit

---
+++ src/test/java/example/NewTest.java
@@ -0,0 +1,31 @@
+package example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	WebDriver webDriver = null;		
+		@Test				
+		public void testEasy() {	
+			webDriver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = webDriver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+			//System.setProperty("webdriver.chrome.driver", "D:\\chrome\\chromedriver.exe");
+			ChromeOptions opt = new ChromeOptions();
+			opt.addArguments("headless");
+			webDriver = new ChromeDriver(opt);
+		}		
+		@AfterTest
+		public void afterTest() {
+			webDriver.quit();			
+		}
+
+}
