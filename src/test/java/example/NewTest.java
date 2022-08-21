e35943b0cac792f46b71fc912c1605b556aa20d4
Wahyu Kurniawan <wahyukurniauan@gmail.com>
2022-08-21 07:19:09 +0700

    first commit 7

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -18,7 +18,7 @@ public class NewTest {
 		}	
 		@BeforeTest
 		public void beforeTest() {	
-			System.setProperty("webdriver.chrome.driver", "D:\\chrome\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "/tools/chromedriver.exe");
 			ChromeOptions opt = new ChromeOptions();
 			opt.addArguments("headless");
 			webDriver = new ChromeDriver(opt);
